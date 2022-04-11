f4fa172e0de7263fb23c1eb84a3b99047b6ac52f
Jakub Daxner <daxoja@gmail.com>
2022-04-11 21:37:53 +0300

    Initial commit

---
+++ src/main/java/net/javaguides/springboot/model/Employee.java
@@ -0,0 +1,30 @@
+package net.javaguides.springboot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id")
+    private String emailId;
+}
