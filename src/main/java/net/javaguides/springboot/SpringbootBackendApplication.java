f0e5789b45ad729ba610680f1985fb4e4b01709b
Jakub Daxner <daxoja@gmail.com>
2022-04-11 22:08:32 +0300

    Frontend settup

--- src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
+++ src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
@@ -1,13 +1,28 @@
 package net.javaguides.springboot;
 
+import net.javaguides.springboot.model.Employee;
+import net.javaguides.springboot.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootBackendApplication {
+public class SpringbootBackendApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
 	}
 
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Employee employee = new Employee();
+		employee.setFirstName("Jakub");
+		employee.setLastName("Daxner");
+		employee.setEmailId("daxoja@gmail.com");
+		employeeRepository.save(employee);
+	}
 }
