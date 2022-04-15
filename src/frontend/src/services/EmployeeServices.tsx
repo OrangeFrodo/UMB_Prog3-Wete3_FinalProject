7a5115a4353906cf8dc32f676513da15a905bd5e
Lukas Haring <lukasharing123@gmail.com>
2022-04-15 22:02:28 +0200

    services for employee

---
+++ src/frontend/src/services/EmployeeServices.tsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const EMPLOYEE_BASE_REST_API_URL = "http://localhost:8080/api/v1/employees";
+
+// Services class to handle all the REST API calls
+class EmployeeService {
+    getAllEmployees() {
+        return axios.get(EMPLOYEE_BASE_REST_API_URL);
+    }
+
+    createEmployee(employee: any) {
+        return axios.post(EMPLOYEE_BASE_REST_API_URL, employee);
+    }
+
+    getEmployeeById(id: any) {
+        return axios.get(EMPLOYEE_BASE_REST_API_URL + "/" + id);
+    }
+
+    updateEmployee(id: any, employee: Object) {
+        return axios.put(EMPLOYEE_BASE_REST_API_URL + "/" + id, employee)
+    }
+
+    deleteEmployee(id: any){
+        return axios.delete(EMPLOYEE_BASE_REST_API_URL + "/" + id)
+    }
+}
+
+export default new EmployeeService();
